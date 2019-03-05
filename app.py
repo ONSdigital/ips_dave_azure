6450ce0191450c71ae80f0a733f82d93d92e66fe
David Powell <davidpowell@C02QV2P8FVH6.local>
2019-03-05 11:07:05 +0000

    mysql

--- app.py
+++ app.py
@@ -1,5 +1,15 @@
-from flask import Flask
+from flask import Flask,request
+from flaskext.mysql import MySQL
+
+
+mysql = MySQL()
 app = Flask(__name__)
+app.config['MYSQL_DATABASE_USER'] = 'poweld2'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'password'
+app.config['MYSQL_DATABASE_DB'] = 'empdata'
+app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+app.config['MYSQL_DATABASE_PORT'] = 3307
+mysql.init_app(app)
 
 
 @app.route("/")
@@ -7,5 +17,20 @@ def hello():
     return "Hello from an Azure Web App running on Linux!"
 
 
+@app.route("/Authenticate")
+def authenticate():
+    username = request.args.get('UserName')
+    password = request.args.get('Password')
+    cursor = mysql.connect().cursor()
+    cursor.execute("SELECT * from sys.User where Username='" + username + "' and Password='" + password + "'")
+    data = cursor.fetchone()
+    if data is None:
+        return "Username or Password is wrong"
+    else:
+        return "Logged in successfully"
+
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
+
+
